4c9f6683076ee41c5f47d0a839b1aa264cfe3b65
ShailySarker <shailysarker99@gmail.com>
2025-02-18 15:57:30 +0600

    live link added

--- src/helpers/config/envConfig.js
+++ src/helpers/config/envConfig.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:1800/api/v1";
+export const BASE_URL = "https://flashcard-learning-app-backend.onrender.com/api/v1";
+// export const BASE_URL = "http://localhost:1800/api/v1";
